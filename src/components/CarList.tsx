cc42c6166d24fefcaa5569066a4b0810706aa770
Šimon Šubík <subs00@vse.cz>
2024-06-02 08:57:39 +0200

    NoRwsults

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,13 +1,16 @@
 import CarItem from "./CarItem";
 import { CarWithDeps } from "@/types/prismaTypes";
+import NoResults from "./NoResults";
 
 type Props = {
   cars: CarWithDeps[];
 };
 
 const CarList = ({ cars }: Props) => {
+  console.log(cars);
   return (
     <div className="px-10">
+      {!cars || (cars.length < 1 && <NoResults />)}
       {cars.map((car) => (
         <CarItem key={car.id} car={car} />
       ))}
---
+++ src/components/NoResults.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoResults = () => {
+  return (
+    <div className="noResultsContainer">
+      <p>No cars found matching your search criteria.</p>
+    </div>
+  );
+};
+
+export default NoResults;
